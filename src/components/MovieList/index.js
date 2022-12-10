816a5bc048cbb8812a5c85ddfa3b957329d843a0
Siti Rosalina <sitirosalina29@gmail.com>
2022-12-10 08:22:02 +0700

    belum fix

--- src/components/MovieList/index.js
+++ src/components/MovieList/index.js
@@ -1,32 +1,30 @@
-import React from "react";
-import "../../App.css";
-import IcPlay from "../../assets/icons/ic_play.svg";
 
-const MovieList = (props) => {
-  return (
-    <>
-      {props.movies.map((movie, index) => (
-        <div className="relative group overflow-hidden mr-[30px]">
-          <img
-            src={movie.Poster}
-            className="object-cover rounded-[30px] h-[340px] w-[250px]"
-            alt=""
-          />
-          <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black rounded-bl-[28px] rounded-br-[28px]">
-            <div className="px-7 pb-7">
-              <div className="font-medium text-xl text-white">{movie.Title}</div>
-            </div>
-          </div>
-          <div
-            className="absolute top-1/2 left-1/2 -translate-y-[500px] group-hover:-translate-y-1/2 -translate-x-1/2 z-20 transition ease-in-out duration-500"
-          >
-            <img src={IcPlay} className="" width="50" alt="" />
-          </div>
-          <a href="#" className="inset-0 absolute z-50"></a>
-        </div>
+
+import MoviesGridContainer from './MoviesGridContainer';
+import MovieListItem from './MovieListItem';
+import Pagination from 'components/Pagination';
+import withTheme from 'utils/hocs/withTheme';
+
+const MovieList = ({
+  theme,
+  movies,
+  baseUrl
+}) => (
+  <>
+    <MoviesGridContainer theme={theme}>
+      {movies.results.map((movie, index) => (
+        <MovieListItem
+          theme={theme}
+          key={movie.id}
+          movie={movie}
+          fetchpriority={index === 0? "high" : "low"}
+          baseUrl={baseUrl} />
       ))}
-    </>
-  );
-};
+    </MoviesGridContainer>
+    <Pagination
+      page={movies.page}
+      totalPages={movies.total_pages} />
+  </>
+);
 
-export default MovieList;
+export default withTheme(MovieList);
